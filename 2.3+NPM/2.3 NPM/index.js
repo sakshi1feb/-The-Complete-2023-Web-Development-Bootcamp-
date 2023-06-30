e6d1c1f10b739411f653c6d8b0ad63499f0cc38e
sakshi1feb <sakshi1feb@gmail.com>
2023-06-30 10:00:10 +0530

    learning npm

---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,15 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+// console.log(`My new Name is ${sillyName}.`)
+
+
+import   from 'superheroes'
+// const superheroes = require('superheroes');
+
+// /superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+
+ var randomName = superheroes.random();
+//=> 'Spider-Ham'
+
+console.log(`My fav superhero is ${randomName}`);
